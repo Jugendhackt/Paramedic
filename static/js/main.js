02cf50d9e78628d4553b70500331049282e80c36
konradrundfunk <konrad@eichsfeld.net>
2022-04-24 10:11:31 +0200

    threading works

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,13 @@
-if ('serviceWorker' in navigator) {
-    console.log("hello")
-    navigator.serviceWorker.register('static/js/worker.js')
-    .then(function(registration) {
-      console.log('Registration successful, scope is:', registration.scope);
-    })
-    .catch(function(error) {
-      console.log('Service worker registration failed, error:', error);
-    });
-  }
-  
+window.addEventListener('load', () => {
+    
+    if ('serviceWorker' in navigator) {
+        console.log("hello")
+        navigator.serviceWorker.register('/static/js/worker.js')
+        .then(function(registration) {
+        console.log('Registration successful, scope is:', registration.scope);
+        })
+        .catch(function(error) {
+        console.log('Service worker registration failed, error:', error);
+        });
+    }
+})
